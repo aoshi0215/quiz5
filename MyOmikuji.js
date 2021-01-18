7eb48d78f9b73f195e1ebba0d3f07cc8bc411072
aoshi0215 <51108018+aoshi0215@users.noreply.github.com>
2021-01-18 13:57:23 +0900

    Add files via upload

---
+++ MyOmikuji.js
@@ -0,0 +1,391 @@
+const quiz = [
+    {
+        question: 'SNSは何の略？' ,
+        answers: [
+            'ソーシャルネットワークシステム',
+            'ソーシャルネットワーキングシステム',
+            'ソーシャルネットワークサービス',
+            'ソーシャルネットワーキングサービス'
+        ],
+        correct: "ソーシャルネットワーキングサービス"
+    },{
+        question: '小泉純一郎の出身大学はどれ？',
+        answers: [
+            '東京大学',
+            '一橋大学',
+            '慶應義塾大学',
+            '早稲田大学',
+        ],
+        correct: '慶應義塾大学'
+    },{
+        question: 'Facebookの創始者は誰？',
+        answers: [
+            'スティーブ・ジョブズ',
+            'マーク・ザッカーバーグ',
+            'ビル・ゲイツ',
+            'ジョフ・ペゾス',
+        ],
+        correct: 'マーク・ザッカーバーグ'
+    },{
+        question: '「国富論を書いた経済学者は誰？',
+        answers: [
+            'ケインズ',
+            'アダムスミス',
+            'フリードマン',
+            'ウィルスミス',
+        ],
+        correct: 'アダムスミス'
+    },{
+        question: '「ピアノの詩人と呼ばれるのは誰？',
+        answers: [
+            'バッハ',
+            'モーツアルト',
+            'ショパン',
+            'ベートーベン',
+        ],
+        correct: 'ショパン'
+    },{
+        question: '2015年に結ばれたco2削減に関するものは？',
+        answers: [
+            '東京議定書',
+            '京都議定書',
+            '上海協定',
+            'パリ協定',
+        ],
+        correct: 'パリ協定'
+    },{
+        question: '以下のうち最高情報責任者をあらわすのはどれ？',
+        answers: [
+            'CTO',
+            'CEO',
+            'CFO',
+            'CIO',
+        ],
+        correct: 'CIO'
+    },{
+        question: '日本人がまだ受賞していないノーベル賞は？',
+        answers: [
+            '生理学',
+            '医学',
+            '経済学',
+            '平和',
+        ],
+        correct: '経済学'
+    },{
+        question: 'アメリカの中央銀行の略称は？',
+        answers: [
+            'BIS',
+            'FRB',
+            'CTR',
+            'PCR',
+        ],
+        correct: 'FRB'
+    },{
+        question: '格安航空会社のことを何という？',
+        answers: [
+            'LBM',
+            'ANA',
+            'LLC',
+            'BBA',
+        ],
+        correct: 'LLC'
+    },{
+        question: '環太平洋経済パートナーシップのことをなんという？',
+        answers: [
+            'FTA',
+            'NAFTA',
+            'FTP',
+            'TPP',
+        ],
+        correct: 'TPP'
+    },{
+        question: '放射能の量を表す単位は？',
+        answers: [
+            'デシベル',
+            'ベクトル',
+            'シーベルト',
+            'あああああ',
+        ],
+        correct: 'ベクトル'
+    },{
+        question: '大陸間弾道弾のことをなんという?',
+        answers: [
+            'IAEA',
+            'IRBM',
+            'ICBM',
+            'IIII',
+        ],
+        correct: 'ICBM'
+    },{
+        question: '一番最年少なのは誰？',
+        answers: [
+            'スティーブ・ジョブズ',
+            'マーク・ザッカーバーグ',
+            'ビル・ゲイツ',
+            '青木',
+        ],
+        correct: '青木'
+    },{
+        question: '青木の好きなたべものは？',
+        answers: [
+            'じゃがりこ',
+            '堅あげポテト',
+            'チョコレート',
+            'ハイチュウ',
+        ],
+        correct: 'ハイチュウ'
+    },{
+        question: '青木の身長は？',
+        answers: [
+            '195',
+            '185',
+            '175',
+            '205',
+        ],
+        correct: '165'
+    },{
+        question: '世界で最も長い川は？',
+        answers: [
+            'ミシシッピ川',
+            'ナイル川',
+            'アマゾン川',
+            '信濃川',
+        ],
+        correct: 'ナイル川'
+    },{
+        question: '純粋理性批判で有名なドイツ観念論哲学の創始者は？',
+        answers: [
+            'ベーコン',
+            'エンゲルス',
+            'ニーチェ',
+            'カント',
+        ],
+        correct: 'カント'
+    },{
+        question: 'ペニシリンを発見したのは誰？',
+        answers: [
+            'ダーウィン',
+            'パスツール',
+            'フレミング',
+            'ジェンナー',
+        ],
+        correct: 'フレミング'
+    },{
+        question: 'IPS細胞の開発に携わった日本人は？',
+        answers: [
+            '山中伸弥',
+            '湯川秀樹',
+            '北里柴三郎',
+            '青木慶彦',
+        ],
+        correct: '山中伸弥'
+    },{
+        question: '次のうち世界三大料理ではないものはどれ？',
+        answers: [
+            'フランス料理',
+            '日本料理(和食）',
+            'トルコ料理',
+            '中華料理',
+        ],
+        correct: '日本料理(和食）'
+    },{
+        question: '石油産出国が加盟している「石油輸出機構」の略称は？',
+        answers: [
+            'NAFTA',
+            'ASEAN',
+            'OPEC',
+            'BRICs',
+        ],
+        correct: 'OPEC'
+    },{
+        question: '日本の領海は何海里？',
+        answers: [
+            '10',
+            '11',
+            '12',
+            '13',
+        ],
+        correct: '12'
+    },{
+        question: '1+1は？',
+        answers: [
+            '1',
+            '田んぼの田',
+            '2',
+            '0',
+        ],
+        correct: '2'
+    },{
+        question: '日本の国会本会議の決議条件は議員の何割以上の賛成が必要か？',
+        answers: [
+            '1/4',
+            '1/3',
+            '1/2',
+            '2/3',
+        ],
+        correct: '1/2'
+    },{
+        question: '衆議院議員の被選挙権（立候補）の年齢は？',
+        answers: [
+            '18',
+            '20',
+            '25',
+            '30',
+        ],
+        correct: '25'
+    },{
+        question: 'サンフランシスコ平和条約を締結した時の日本の首相は？',
+        answers: [
+            '吉田茂',
+            '吉田慧人',
+            '吉田松陰',
+            '吉田将人',
+        ],
+        correct: '吉田茂'
+    },{
+        question: '「人名は地球よりも重い」と発言しテロリストの要求をのんだ日本の首相は？',
+        answers: [
+            '福田武夫',
+            '福田福',
+            '福田大福',
+            'いちご大福',
+        ],
+        correct: '福田武夫'
+    },{
+        question: '国民総生産をあらわす略語はどれ？',
+        answers: [
+            'GDP',
+            'GNP',
+            'GPS',
+            'GOD',
+        ],
+        correct: 'GNP'
+    },{
+        question: '家計の支出に占める食費の割合をなんという？',
+        answers: [
+            'エンゲル係数',
+            'エンジェル係数',
+            'エランゲル係数',
+            'エリンギ係数',
+        ],
+        correct: 'エンゲル係数'
+    },{
+        question: '1976年にブータン国王の提唱した国民総幸福量の名称は？',
+        answers: [
+            'GNH',
+            '幸せは量では表せない',
+            'ハッピーターン',
+            'ぶーｗｗｗ',
+        ],
+        correct: 'GNH'
+    },{
+        question: '1971年の金ドル交換停止のさいにおこったショックを何という？',
+        answers: [
+            '電気ショック',
+            'スプートニクショック',
+            'ニクソンショック',
+            'ハックション',
+        ],
+        correct: 'ニクソンショック'
+    },{
+        question: '「雇用・利子および貨幣の一般理論」を著した経済学者は？',
+        answers: [
+            'アダムスミス',
+            'ウィル・スミス',
+            'レオナルドディカプリオ',
+            'ケインズ',
+        ],
+        correct: 'ケインズ'
+    },{
+        question: '国債の発行に肯定的な現代貨幣理論のことをなんという？',
+        answers: [
+            'TPP理論',
+            'MMT理論',
+            'WWW理論',
+            'uhh理論',
+        ],
+        correct: 'MMT理論'
+    },{
+        question: 'ビジネスにおけるCFOとは？',
+        answers: [
+            '最高経営責任者',
+            '最高執行責任者',
+            '最高情報責任者',
+            '最高財務責任者',
+        ],
+        correct: '最高財務責任者'
+    },{
+        question: '企業経営において、業務を外部委託することをなんという？',
+        answers: [
+            'クラウドソーシング',
+            'アウトソーシング',
+            'アウトソーナンス',
+            'クリームソース',
+        ],
+        correct: 'アウトソーシング'
+    },{
+        question: 'コンピュータの計算機能の中枢となるICである中央演算装置のことをなんという？',
+        answers: [
+            'CPU',
+            'OS',
+            'ITC',
+            'LAN'
+        ],
+        correct: 'CPU'
+    }
+
+];
+
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
+
+
+const $button = document.getElementsByTagName('button');
+
+const buttonLength = $button.length;
+
+
+//　クイズの問題文、選択肢を定義
+
+const setupQuiz = () => {
+ let buttonIndex = 0;
+ while(buttonIndex < buttonLength){
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+    buttonIndex ++;
+ }
+}
+
+setupQuiz();
+
+
+const clickHandler = (e) => {
+    if(quiz[quizIndex].correct === e.target.textContent){
+        window.alert('正解！');
+        score++;
+    }else{
+        window.alert('不正解！答えは' + quiz[quizIndex].correct + 'でした！');
+    }
+
+    quizIndex++
+
+    if(quizIndex < quizLength){
+        //　問題数がまだあればこちらを実行
+        setupQuiz();
+    } else {
+        //　問題数がもうなければこちらを実行
+        window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+    }
+
+};
+
+// ボタンをクリックしたら正誤判定
+
+let handlerIndex = 0;
+while(handlerIndex < buttonLength){
+    $button[handlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
